71b593a783c47cd57e8b1c12d7c01d870b4f0c22
brenda70904 <brendaj0214@gmail.com>
2022-10-17 21:36:26 -0700

    update JS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 'use strict';
+let container = document.getElementById('imageSection');
+let results = document.querySelector('ul');
 
+
+let p1 = document.getElementById('p1');
+let p2 = document.getElementById('p2');
+let p3 = document.getElementById('p3');
 //1.create image object to store all the info
 
 function Product(name, fileExtenstion = 'jpeg') {
@@ -33,9 +39,8 @@ let wineGlass = new Product('wineGlass');
 
 
 //2. make object array
-let allProducts = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulu', 'dogDuck', 'dragon', 'ducklogo', 'pen', 'petSweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'waterCan', 'wineGlass'];
+let allProducts = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulu, dogDuck, dragon, ducklogo, pen, petSweep, scissors, shark, sweep, tauntaun, unicorn, waterCan, wineGlass];
 
-console.log(allProducts);
 //3.randomly select the image
 function randomPick() {
   return Math.floor(Math.random() * allProducts.length);
@@ -47,10 +52,37 @@ function renderImage() {
   while (p1 === p2 || p1 === p3 || p2 === p3) {
     p2 = randomPick();
     console.log(p1, p2, p3);
-
   }
+  p1.src = allProducts[p1].src;
+  p1.alt = allProducts[p1].name;
+  allProducts[p1]++;
+  p2.src = allProducts[p2].src;
+  p2.alt = allProducts[p2].name;
+  allProducts[p2]++;
+  p3.src = allProducts[p3].src;
+  p3.alt = allProducts[p3].name;
+  allProducts[p3]++;
 }
 console.log(renderImage());
+
+function renderResults() {
+  for (let i = 0; i < allProducts.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = `${allProducts[i].name} had ${allProducts[i].view} views and ${allProducts[i].score} votes.`;
+  }
+}
+
+function handleClick(event) {
+  if (event.target === container) {
+    alert('Check results');
+  }
+  console.log(event.target.alt);
+
+
+}
+
+container.addEventListener('click',handleClick);
+renderImage();
 //4. connect to DOM
 
 //5. add score and view
